297020945251c2eeef182fa211ab7fcd36a0a5f6
ICT <ict@R1-00.local>
2012-07-24 20:52:54 +0900

    first commit

---
+++ src/sample/application/memopad/TestActivity.java
@@ -0,0 +1,50 @@
+package sample.application.memopad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onStop();
+	}
+
+}
